53b949c8b1becd1a85233671ce3efcbde3b6c44b
Akoz <jedealbagaytan@gmail.com>
2023-11-01 16:26:54 -0700

    fix get errors

--- app/routes/controllers/batch/get.py
+++ app/routes/controllers/batch/get.py
@@ -3,6 +3,14 @@ from services.db import colina_db
 
 
 async def get_batch_in_dev(development_id:int,):
+    dev_db = colina_db.fetch_one(
+        sql="SELECT id FROM developments WHERE id = %s",
+        params=(development_id,)
+    )
+
+    if not dev_db:
+        raise HTTPException(status_code=404, detail="Development not found")
+
     batch_db = colina_db.fetch_all(
         sql="SELECT * FROM batches WHERE development_id = %s",
         params=(development_id,)
@@ -10,8 +18,8 @@ async def get_batch_in_dev(development_id:int,):
 
     if not batch_db:
         raise HTTPException(status_code=404, detail="Batch not found")
-    
-    return batch_db
 
-    
-    
+    return {
+        'message': 'Batch found successfully',
+        'data': batch_db
+    }
