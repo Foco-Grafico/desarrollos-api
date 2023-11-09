a420dd09203299d84e1033277bc5fd3d76cafecd
Akoz <jedealbagaytan@gmail.com>
2023-11-09 16:40:34 -0700

    get

--- app/routes/controllers/batch/get.py
+++ app/routes/controllers/batch/get.py
@@ -1,5 +1,6 @@
 from fastapi import HTTPException
 from services.db import colina_db
+import math
 
 async def get_batches():
     batches = colina_db.fetch_all(
@@ -28,7 +29,7 @@ async def get_batches():
         'data': batches
     }
 
-async def get_batch_in_dev(development_id:int,):
+async def get_batch_in_dev(development_id:int, elements: int = 50, page: int = 1):
     dev_db = colina_db.fetch_one(
         sql="SELECT id FROM developments WHERE id = %s",
         params=(development_id,)
@@ -38,8 +39,8 @@ async def get_batch_in_dev(development_id:int,):
         raise HTTPException(status_code=404, detail="Development not found")
 
     batch_db = colina_db.fetch_all(
-        sql="SELECT b.*, UUID() as `key` FROM batches b WHERE development_id = %s",
-        params=(development_id,)
+        sql="SELECT b.*, UUID() as `key` FROM batches b WHERE development_id = %s LIMIT %s OFFSET %s",
+        params=(development_id, elements, (page - 1) * elements)
     )
 
     if not batch_db:
@@ -55,8 +56,20 @@ async def get_batch_in_dev(development_id:int,):
             sql="SELECT * FROM batch_payment_plans WHERE batch_id = %s",
             params=(batch['id'],)
         )
-        
+
+        batch['status'] = colina_db.select_one(
+            table='batch_status',
+            where={'id': batch['status']},
+            columns=['*']
+        )
+
     return {
         'message': 'Batch found successfully',
-        'data': batch_db
+        'data': batch_db,
+        'max_pages': math.ceil(colina_db.select_one(
+            table='batches',
+            where={'development_id': development_id},
+            columns=['count(*) as counter']
+        )['counter'] / elements),
+        'page': page
     }
