1d11be9049157d5714fef48c8b2d0939bb3a174f
Akoz <jedealbagaytan@gmail.com>
2023-12-08 23:57:09 -0700

    mejoras en la funcion de whatsapp y setTimeout

--- app/utils/intervals.py
+++ app/utils/intervals.py
@@ -17,3 +17,19 @@ class setInterval:
     def cancel(self, timeout: float = 0):
         t = threading.Timer(timeout, self.stopEvent.set)
         t.start()
+
+class setTimeout:
+    def __init__(self, interval: float, action):
+        self.interval = interval
+        self.action = action
+        self.stopEvent = threading.Event()
+        thread = threading.Thread(target=self.__setTimeout)
+        thread.start()
+
+    def __setTimeout(self):
+        time.sleep(self.interval)
+        self.action(self)
+
+    def cancel(self, timeout: float = 0):
+        t = threading.Timer(timeout, self.stopEvent.set)
+        t.start()
--- services/whatsapp/messages.py
+++ services/whatsapp/messages.py
@@ -1,7 +1,9 @@
-from services.whatsapp.models.wa_models import Interactive, Msg, MsgUser, Contact, Button, Buttons
+from services.whatsapp.models.wa_models import Interactive, Msg, MsgUser, Contact, Button, Buttons, List, ListElement, SectionList, Header, HeaderTypes
 from services.whatsapp.enums.wa_enums import MsgTypes
 from services.whatsapp.controller.send import send_controller, mark_as_read_message
 from services.db import colina_db
+from app.utils.intervals import setTimeout, setInterval
+from random import randint
 import re
 
 def format_message(msg) -> Msg | None:
@@ -59,7 +61,7 @@ def receive_message(u_msg):
                 client_name = match.group(3)
 
                 seller = colina_db.fetch_one(
-                    sql='SELECT phone FROM sellers WHERE id IN (SELECT seller_id FROM batch_sellers WHERE batch_id = %s AND client_phone = %s)',
+                    sql='SELECT phone, id FROM sellers WHERE id IN (SELECT seller_id FROM batch_sellers WHERE batch_id = %s AND client_phone = %s)',
                     params=(batch_id, client_phone)
                 )
 
@@ -111,19 +113,201 @@ def receive_message(u_msg):
                 send_controller(client, msg.user.number)
                 send_controller(
                     to=msg.user.number,
+                    message=f'Se te ha asignado un nuevo cliente: {client.name} - {client.phone}\n.En aproximadamente un minuto podras interactuar con el apartado del lote.\npdf: {pdf_formatted}'
+                )
+
+                send_controller('Te hemos asignado un vendedor, se contactara contigo en unos instantes\n pdf: {pdf_formatted}', client_phone)
+
+                setTimeout(60, lambda: send_controller(
+                    to=client_phone,
                     message=Buttons(
-                        body=f'Se te ha asignado un nuevo cliente: {client.name} - {client.phone}',
+                        body=f'¿Te ha contactado el vendedor?',
                         buttons=[
                             Button(
-                                execute=f'batch_appart batch"{batch_id}"',
-                                title='Apartar lote'
+                                execute=f'is_contact batch"{batch_id}" client_n"{client_name}" seller_id"{seller["id"]}',
+                                title='Si'
+                            ),
+                            Button(
+                                execute=f'is_contact batch"{batch_id}" client_n"{client_name}"',
+                                title='No'
+                            )
+                        ]
+                    )
+                ))
+
+        if name == 'is_contact':
+            match = re.search(r'batch"(\d+)" client_n"(.+?)" seller_id"(\d+)"', msg.message.id)
+
+            if match:
+                batch_id = match.group(1)
+                client_name = match.group(2)
+                seller_id = match.group(3)
+
+                if msg.message.title == 'No':
+                    seller = colina_db.fetch_one(
+                        sql='SELECT phone FROM sellers WHERE id = %s',
+                        params=(seller_id,)
+                    )
+
+                    send_controller(
+                        to=seller['phone'],
+                        message=f'El cliente {client_name} ha dicho que no lo has contactado, si esto es falso, por favor contacta con un moderador.'
+                    )
+
+                    send_seller_message = lambda seller_phone: send_controller(
+                        to=seller_phone,
+                        message=Buttons(
+                            body='Hola, te presento a un nuevo cliente potencial',
+                            buttons=[
+                                Button(
+                                    execute=f'accept_seller batch"{batch_id}" client_p"{msg.user.number}" client_n"{client_name}"',
+                                    title='Aceptar'
+                                )
+                            ],
+                            footer='Tienes 5 min para aceptar.'
+                        )
+                    )
+
+                    batch = colina_db.select_one(
+                        table='batches',
+                        where={
+                            'id': batch_id
+                        },
+                        columns=['development_id']
+                    )
+
+                    sellers = colina_db.fetch_all(
+                        sql='SELECT * FROM sellers WHERE id IN (SELECT seller_id FROM developments_sellers WHERE development_id = %s)',
+                        params=(batch['development_id'],)
+                    )
+
+                    get_random_seller = lambda: sellers[randint(0, len(sellers) - 1)]
+
+                    send_controller('De acuerdo, buscaremos otro vendedor para ti.', msg.user.number)
+
+                    random_seller = get_random_seller()
+                    send_seller_message(random_seller['phone'])
+
+                    colina_db.update(
+                        table='batch_sellers',
+                        data={
+                            'status': 'pending',
+                            'seller_id': random_seller['id']
+                        },
+                        where=f'batch_id = {batch_id} AND client_name = {client_name}'
+                    )
+
+                    def action(inter: setInterval):
+                        batch_seller = colina_db.fetch_one(
+                            sql='SELECT status FROM batch_sellers WHERE batch_id = %s AND client_phone = %s',
+                            params=(batch_id, msg.user.number)
+                        )
+
+                        if batch_seller['status'] == 'pending':
+                            new_random_seller = get_random_seller()
+                            
+                            while new_random_seller['id'] == random_seller['id']:
+                                new_random_seller = get_random_seller()
+
+                            send_seller_message(new_random_seller['phone'])
+                            colina_db.update(
+                                table='batch_sellers',
+                                data={
+                                    'seller_id': new_random_seller['id']
+                                },
+                                where=f'batch_id = {batch_id} AND client_phone = {msg.user.number}'
+                            )
+                        else:
+                            inter.cancel()
+
+                    setInterval(
+                        interval=20,
+                        action=action
+                    )
+                    return
+
+                batch_sell = colina_db.select_one(
+                    table='batch_sellers',
+                    where={
+                        'batch_id': batch_id,
+                        'client_phone': msg.user.number
+                    },
+                    columns=['*']
+                )
+
+                seller = colina_db.select_one(
+                    table='sellers',
+                    where={
+                        'id': seller_id
+                    },
+                    columns=['*']
+                )
+
+                if not seller:
+                    send_controller('Parece que ha ocurrido un error y no hemos encontrado a tu vendedor.', msg.user.number)
+                    return
+                
+                send_controller(
+                    to=seller['phone'],
+                    message=f'El cliente {client_name} ha dicho que lo has contactado, si esto es falso, por favor contacta con un moderador.'
+                )
+
+                send_controller(
+                    to=seller['phone'],
+                    message=Buttons(
+                        body=f'Te otorgamos un boton para bloquear el lote, precionalo al recibir el apartado del mismo.\nEn caso de error, consulta con un moderador.',
+                        buttons=[
+                            Button(
+                                execute=f'block seller_id"{seller["id"]}" batch"{batch_id}" client_n"{client_name}"',
+                                title='Bloquear'
                             )
                         ]
                     )
                 )
 
-                send_controller('un vendedor se pondra en contacto rightnow', client_phone)
-
+                send_controller(
+                    to=msg.user.number,
+                    message=List(
+                        header=Header(
+                            type=HeaderTypes.text,
+                            text='Gracias por tu respuesta'
+                        ),
+                        body=f'Califica tu experiencia con el vendedor {seller["name"]}',
+                        sections=[
+                            SectionList(
+                                title='Calificacion',
+                                rows=[
+                                    ListElement(
+                                        execute=f'seller_quality quality"1" seller_id"{seller["id"]}"',
+                                        title='Muy malo',
+                                        description='⭐'
+                                    ),
+                                    ListElement(
+                                        execute=f'seller_quality quality"2" seller_id"{seller["id"]}"',
+                                        title='Malo',
+                                        description='⭐⭐'
+                                    ),
+                                    ListElement(
+                                        execute=f'seller_quality quality"3" seller_id"{seller["id"]}"',
+                                        title='Regular',
+                                        description='⭐⭐⭐'
+                                    ),
+                                    ListElement(
+                                        execute=f'seller_quality quality"4" seller_id"{seller["id"]}"',
+                                        title='Bueno',
+                                        description='⭐⭐⭐⭐'
+                                    ),
+                                    ListElement(
+                                        execute=f'seller_quality quality"5" seller_id"{seller["id"]}"',
+                                        title='Muy bueno',
+                                        description='⭐⭐⭐⭐⭐'
+                                    )
+                                ]
+                            )
+                        ],
+                        button='Calificar'
+                    )
+                )
 
 
     # send_controller('buenas', msg.user.number)
